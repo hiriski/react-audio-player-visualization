7b89ddd695e09dfc8d5fb1cd4366964126ce8ca8
hiriski <hiriski@outlook.com>
2023-08-12 20:10:26 +0700

    style: setup material ui theme

---
+++ src/theme/config/theme.palette-base.ts
@@ -0,0 +1,18 @@
+import { PaletteOptions } from '@mui/material'
+
+const paletteBase: Partial<PaletteOptions> = {
+  primary: {
+    light: '#D8EEFF',
+    main: '#3C8BFF',
+    dark: '#2B6BDB',
+    contrastText: '#fbfbfb'
+  },
+  secondary: {
+    light: '#FFF4CC',
+    main: '#ffa800',
+    dark: '#DB8800',
+    contrastText: '#fbfbfb'
+  }
+}
+
+export { paletteBase }
