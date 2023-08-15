4100b4d13a5db9d5ce520feccb415b18bfed3b1f
hiriski <hiriski@outlook.com>
2023-08-15 22:02:07 +0700

    fix: missing filename

--- src/data.ts
+++ src/data.ts
@@ -13,16 +13,16 @@ export const mediaList: IMedia[] = [
     id: 2,
     title: 'Waiting For You',
     artist: 'MONO',
-    cover_image_url: 'waiting_for_you_cover.jpg',
-    file_url: '/media/waiting_for_you.mp3',
+    cover_image_url: '2.jpg',
+    file_url: '/media/2.mp3',
     mime_type: 'audio/mpeg'
   },
   {
     id: 3,
     title: "Wish You'd Make Me Cry",
     artist: '(Nightcore) UPSAHL',
-    cover_image_url: '/Wish_You_Make_Me_Cry.png',
-    file_url: '/media/Wish_You_Make_Me_Cry.mp3',
+    cover_image_url: '/3.png',
+    file_url: '/media/3.mp3',
     mime_type: 'audio/mpeg'
   }
 ]
